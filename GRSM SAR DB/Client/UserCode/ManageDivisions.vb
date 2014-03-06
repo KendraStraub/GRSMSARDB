17fb23cd1816121305016d93d73deee49a527750
tpcolson <thomas_colson@nps.gov>
2014-03-06 08:31:46 -0500

    Editing Mange Users Screen

---
+++ GRSM SAR DB/Client/UserCode/ManageDivisions.vb
@@ -0,0 +1,19 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class ManageDivisions
+
+        Private Sub ManageDivisions_InitializeDataWorkspace(ByVal saveChangesTo As Global.System.Collections.Generic.List(Of Global.Microsoft.LightSwitch.IDataService))
+            ' Write your code here.
+            Me.ParkDivisionProperty = New ParkDivision()
+        End Sub
+
+        Private Sub ManageDivisions_Saved()
+            ' Write your code here.
+            Me.Close(False)
+            Application.Current.ShowDefaultScreen(Me.ParkDivisionProperty)
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ GRSM SAR DB/Client/UserCode/ManageResponseZones.vb
@@ -0,0 +1,19 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class ManageResponseZones
+
+        Private Sub ManageResponseZones_InitializeDataWorkspace(ByVal saveChangesTo As Global.System.Collections.Generic.List(Of Global.Microsoft.LightSwitch.IDataService))
+            ' Write your code here.
+            Me.ResponseZonesProperty = New ResponseZones()
+        End Sub
+
+        Private Sub ManageResponseZones_Saved()
+            ' Write your code here.
+            Me.Close(False)
+            Application.Current.ShowDefaultScreen(Me.ResponseZonesProperty)
+        End Sub
+
+    End Class
+
+End Namespace
