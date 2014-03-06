0ca86a76eb2fe91281b706157d62af2afcba5d8f
tpcolson <thomas_colson@nps.gov>
2014-03-06 05:11:41 -0500

    Added certification edit screen

--- GRSM SAR DB/Common/UserCode/xref_EmployeeCerts.vb
+++ GRSM SAR DB/Common/UserCode/xref_EmployeeCerts.vb
@@ -4,8 +4,8 @@ Namespace LightSwitchApplication
     Public Class xref_EmployeeCerts
 
         Private Sub Summary_Compute(ByRef result As String)
-            If (CertificationItem IsNot Nothing) Then
-                result = CertificationItem.Certification
+            If (Certifications IsNot Nothing) Then
+                result = Certifications.Certification
             End If
         End Sub
     End Class
