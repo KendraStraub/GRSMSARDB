fbeb43fc742925a68d8d2d89cfa983a6be7bc369
tpcolson <thomas_colson@nps.gov>
2014-03-06 16:34:29 -0500

    Oops again

---
+++ GRSM SAR DB/Common/UserCode/xref_EmployeeTechCert.vb
@@ -0,0 +1,13 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class xref_EmployeeTechCert
+
+        Private Sub Summary_Compute(ByRef result As String)
+            If (TechRescueCerts IsNot Nothing) Then
+                result = TechRescueCerts.Certification
+            End If
+        End Sub
+    End Class
+
+End Namespace
