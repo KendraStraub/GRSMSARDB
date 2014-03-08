b75614508d4f607334f02c71c76f137f07d26cb3
tpcolson <thomas_colson@nps.gov>
2014-03-08 09:13:33 -0500

    Converted Employee->TECHCERT from many to many to 0:1 to many relationship

--- GRSM SAR DB/Client/UserCode/ManageTeamMembers.vb
+++ GRSM SAR DB/Client/UserCode/ManageTeamMembers.vb
@@ -26,27 +26,7 @@ Namespace LightSwitchApplication
             End If
         End Sub
 
-        Private Sub AddTechCert_Execute()
-            If (Employees.SelectedItem IsNot Nothing And AllTechCertifications.SelectedItem IsNot Nothing) Then
-                ' Loop through the category list and see if we already have this category
-                ' If so, don't allow it to be added (again)
-                For Each c In xref_EmployeeTechCerts
-                    If (c.TechRescueCerts Is AllTechCertifications.SelectedItem) Then
-                        Exit Sub
-                    End If
-                Next
-                ' Add the new category to the category list
-                Dim cc As xref_EmployeeTechCert = xref_EmployeeTechCerts.AddNew()
-                cc.Employee = Employees.SelectedItem
-                cc.TechRescueCerts = AllTechCertifications.SelectedItem
-            End If
-        End Sub
 
-        Private Sub DeleteTechCert_Execute()
-            If (xref_EmployeeTechCerts.SelectedItem IsNot Nothing) Then
-                xref_EmployeeTechCerts.SelectedItem.Delete()
-            End If
-        End Sub
 
         Private Sub AddWaterCertification_Execute()
             If (Employees.SelectedItem IsNot Nothing And AllWaterRescueCerts.SelectedItem IsNot Nothing) Then
