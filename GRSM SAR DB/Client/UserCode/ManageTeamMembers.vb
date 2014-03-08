a7c0d74cdd0c1f88c3d0dd3fa38e1f9a3a829631
tpcolson <thomas_colson@nps.gov>
2014-03-08 09:08:10 -0500

    Converted Employee->SARCERT from many to many to 0:1 to many relationship

--- GRSM SAR DB/Client/UserCode/ManageTeamMembers.vb
+++ GRSM SAR DB/Client/UserCode/ManageTeamMembers.vb
@@ -3,27 +3,6 @@ Namespace LightSwitchApplication
 
     Public Class ManageTeamMembers
 
-        Private Sub AddCertification_Execute()
-            If (Employees.SelectedItem IsNot Nothing And AllCertifications.SelectedItem IsNot Nothing) Then
-                ' Loop through the category list and see if we already have this category
-                ' If so, don't allow it to be added (again)
-                For Each c In xref_EmployeeCertsCollection
-                    If (c.SARCertificationsSetItem Is AllCertifications.SelectedItem) Then
-                        Exit Sub
-                    End If
-                Next
-                ' Add the new category to the category list
-                Dim cc As xref_EmployeeSARCerts = xref_EmployeeCertsCollection.AddNew()
-                cc.Employee = Employees.SelectedItem
-                cc.SARCertificationsSetItem = AllCertifications.SelectedItem
-            End If
-        End Sub
-
-        Private Sub RemoveCertification_Execute()
-            If (xref_EmployeeCertsCollection.SelectedItem IsNot Nothing) Then
-                xref_EmployeeCertsCollection.SelectedItem.Delete()
-            End If
-        End Sub
 
         Private Sub AddZone_Execute()
             If (Employees.SelectedItem IsNot Nothing And AllResponseZones.SelectedItem IsNot Nothing) Then
