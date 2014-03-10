526c43ec69a1eb83a0ccbdc67c2a19078449357c
tpcolson <thomas_colson@nps.gov>
2014-03-09 22:15:32 -0400

    Testing, some code change

---
+++ GRSM SAR DB/Client/UserCode/EmpoyeeDetail.vb
@@ -0,0 +1,11 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class EmpoyeeDetail
+
+        Private Sub ImportFromExcel_Execute()
+            OfficeIntegration.Excel.Import(Me.Employees)
+        End Sub
+    End Class
+
+End Namespace
