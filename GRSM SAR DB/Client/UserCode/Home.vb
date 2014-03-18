823d82b700ad5bb179b849eb8f5fa147bdb9f23b
tpcolson <thomas_colson@nps.gov>
2014-03-18 15:55:03 -0400

    HomePage

---
+++ GRSM SAR DB/Client/UserCode/Home.vb
@@ -0,0 +1,31 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Home
+
+        Private Sub SARCerts_Execute()
+            Application.ShowManageSARCertifications()
+        End Sub
+
+        Private Sub Divisions_Execute()
+            Application.ShowManageDivisions()
+        End Sub
+
+        Private Sub Zones_Execute(ByRef result As Boolean)
+            Application.ShowManageResponseZones()
+        End Sub
+
+        Private Sub View_Employees_Execute()
+            Application.ShowEmpoyeeDetail()
+        End Sub
+
+        Private Sub AddEmployee_Execute()
+            Application.ShowCreateNewTeamMember()
+        End Sub
+
+        Private Sub EditEmployee_Execute()
+            Application.ShowManageTeamMembers()
+        End Sub
+    End Class
+
+End Namespace
