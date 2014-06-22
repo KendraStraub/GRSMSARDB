c317f59962de7ceb3c9f90eeae478de590c85808
tpcolson <thomas_colson@nps.gov>
2014-06-22 17:52:59 -0400

    '

---
+++ GRSM SAR DB/Client/UserCode/Tracker_IRT.vb
@@ -0,0 +1,23 @@
+﻿
+Namespace LightSwitchApplication
+
+    Public Class Tracker_IRT
+
+        Private Sub Employee_Loaded(succeeded As Boolean)
+            ' Write your code here.
+            Me.SetDisplayNameFromEntity(Me.Employee)
+        End Sub
+
+        Private Sub Employee_Changed()
+            ' Write your code here.
+            Me.SetDisplayNameFromEntity(Me.Employee)
+        End Sub
+
+        Private Sub Tracker_IRT_Saved()
+            ' Write your code here.
+            Me.SetDisplayNameFromEntity(Me.Employee)
+        End Sub
+
+    End Class
+
+End Namespace
